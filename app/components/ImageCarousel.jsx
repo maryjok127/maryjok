7f6946a79aafee533af020175e667fc2fa012808
abhijit@mstindia.com <abhijit@mstindia.com>
2024-05-14 10:47:49 +0530

    banner

--- app/components/ImageCarousel.jsx
+++ app/components/ImageCarousel.jsx
@@ -30,7 +30,7 @@ const ImageCarousel = () => {
   }else{
     imageSrc = [{
       url:"/collections/cult-makeup",
-      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/desktop_banner_1.jpg?v=1713342696'
+      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/mobile_Hero_Banners_1.jpg?v=1713342696'
     },
     {
       img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/mobile_Hero_Banners_2.jpg?v=1713353171',
@@ -112,20 +112,8 @@ const ImageCarousel = () => {
         }
       }
     ]
-    }
-  //'cult-makeup','way-2-slay-mjk',rizz-roll-mjk','pro-studio-mjk',
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  var mobImgHeight= {height:"inherit"}
-  const nextImage = () => {
-    setCurrentImageIndex((currentImageIndex + 1) % images.length);
-  };
+  }
 
-  const prevImage = () => {
-    setCurrentImageIndex(
-      (currentImageIndex - 1 + images.length) % images.length,
-    );
-  };
- 
   return (
     <div className="relative">
       <Slider {...settings}>
@@ -141,83 +129,7 @@ const ImageCarousel = () => {
           ))
         }
       </Slider>
-      {/* <button
-        className="absolute left-0 top-1/2 transform -translate-y-1/2 px-4 py-2 text-white"
-        onClick={() =>
-          setCurrentImageIndex(
-            (currentImageIndex - 1 + images.length) % images.length,
-          )
-        }
-      >
-      <MdChevronLeft
-        className="opacity-50 cursor-pointer hover:opacity-100 mst-arrow"
-        size={40}
-      />
-      </button>
-      <img
-        onClick={()=> goToCollection(currentImageIndex) }
-        className={`lg:w-full md:w-full cursor-pointer sm:w-full object-cover ${isSmall ? 'rounded-2xl' : '' } w-[100%]`}
-        src={images[currentImageIndex]}
-      />
-      <button
-        className="absolute right-0 top-1/2 transform -translate-y-1/2 px-4 py-2 text-white"
-        onClick={() =>
-          setCurrentImageIndex((currentImageIndex + 1) % images.length)
-        }
-      >
-      <MdChevronRight
-        className="opacity-50 cursor-pointer hover:opacity-100 mst-arrow"
-        size={40}
-      />
-      </button> */}
     </div>
   );
 };
 export default ImageCarousel;
-/*
-import React, { useState, useRef } from 'react';
-
-const ImageCarousel = ({ images }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const scrollRef = useRef(null); // Create a ref for the element to scroll to.
-
-  const nextSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
-  const prevSlide = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const scrollToElement = () => {
-    // Scroll to the element with the specified ref.
-    if (scrollRef.current) {
-      scrollRef.current.scrollIntoView({
-        behavior: 'smooth', // You can adjust the scrolling behavior.
-        block: 'start',     // Scrolls to the top of the element.
-      });
-    }
-  };
-
-  return (
-    <div className="overflow-y-auto h-full md:h-80 border border-gray-300 rounded-lg shadow-lg">
-      <div className="flex space-x-4 p-4 group-hover:scrollbar-auto">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image}
-            alt={`Image ${index + 1}`}
-            className="w-full h-full md:w-80 md:h-80 object-cover rounded-lg"
-          />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default ImageCarousel;
-*/
