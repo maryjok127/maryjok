39b274813e86183b16947984649e81be66c25813
abhijit@mstindia.com <abhijit@mstindia.com>
2024-05-24 11:14:58 +0530

    lookbook

--- public/lookbook.js
+++ public/lookbook.js
@@ -2,12 +2,13 @@
 setTimeout(()=>{
     var list = document.querySelectorAll(".ctl-product-link");
     console.log('Lookbook JS',list)
+    if(!list.length) window.location.reload();
     list.forEach((node)=>{
         let oldHref = node.getAttribute('href');
         let rmParams = oldHref.split("?"); 
         var newHref = rmParams[0].replace("https://15f63f.myshopify.com/","/");
         node.setAttribute("href",newHref)
     });
-},1000)
+},700)
 
 
