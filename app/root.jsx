d62f1f59f64f3eceeeccba6a779516cf77b85a08
maryjok127 <167772600+maryjok127@users.noreply.github.com>
2024-07-09 21:47:20 +0530

    Verification.jsx
    For Last Time Have Added this verification code.
    
    Signed-off-by: maryjok127 <167772600+maryjok127@users.noreply.github.com>

--- app/root.jsx
+++ app/root.jsx
@@ -96,9 +96,9 @@ export default function App() {
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
 	    })(window, document, "clarity", "script", "n42g61ijt4");
-	<!-- Google Tag Manager -->
-	    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPDWX7TM');
 	</script>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPDWX7TM');</script>
 	<!-- End Google Tag Manager -->
 	<meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
