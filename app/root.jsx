d114fef1bb1e5eaa20bcdf1dc35aa04ebd9bd48e
maryjok127 <167772600+maryjok127@users.noreply.github.com>
2024-07-09 22:39:36 +0530

    Modified Version of GMT.jsx
    Signed-off-by: maryjok127 <167772600+maryjok127@users.noreply.github.com>

--- app/root.jsx
+++ app/root.jsx
@@ -94,7 +94,12 @@ export default function App() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
 	<meta name="google-site-verification"   content="chuZbQRh2KrTexV9OSYVzpR641zvxEmzEwsHS6pExT0" />
         <meta name="oke:subscriber_id" content="866361a1-6116-4d14-abc8-0565f32d51cb" />
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-TPDWX7TM');</script>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s);j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer');
+	</script>
         <title>Mary Jo K</title>
         <link rel="icon" type="image/x-icon" href="/favicon.png" />
         <link
