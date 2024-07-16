570e51d0a1ed29db25585d5d3c7066efd75541f1
maryjok127 <167772600+maryjok127@users.noreply.github.com>
2024-07-16 21:51:18 +0530

    Google GTM Js Script Added.jsx
    Signed-off-by: maryjok127 <167772600+maryjok127@users.noreply.github.com>

--- app/root.jsx
+++ app/root.jsx
@@ -88,26 +88,14 @@ export default function App() {
   return (
     <html lang="en">
       <head>
-	<Suspense>
-          <script
-            async
-            dangerouslySetInnerHTML={{
-              __html: `{
-              (function (w, d, s, l, i) {
-                  w[l] = w[l] || [];
-                  w[l].push({ "gtm.start": new Date().getTime(), "event": "gtm.js" });
-                  var f = d.getElementsByTagName(s)[0],
-                      j = d.createElement(s),
-                      dl = l != "dataLayer" ? "&l=" + l : "";
-                  j.async = true;
-                  j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-                  f.parentNode.insertBefore(j, f);
-              })(window, document, "script", "dataLayer", "GTM-XXX");
-                ;
-            }`,
-            }}
-          />
-        </Suspense>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-T9FEPNFF7Y"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-T9FEPNFF7Y');
+	</script>
 	<meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
 	<meta name="google-site-verification"   content="chuZbQRh2KrTexV9OSYVzpR641zvxEmzEwsHS6pExT0" />
