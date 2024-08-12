6cc79ca4a82008f68aaa1c7a862f3fa4dd6041f8
maryjok127 <167772600+maryjok127@users.noreply.github.com>
2024-08-12 21:25:33 +0530

    Independence Day.jsx
    Updated independence day banner
    
    Signed-off-by: maryjok127 <167772600+maryjok127@users.noreply.github.com>

--- app/components/ImageCarousel.jsx
+++ app/components/ImageCarousel.jsx
@@ -12,8 +12,12 @@ const ImageCarousel = () => {
   var imageSrc = []
   if(!isSmall){
     imageSrc = [{
+      url:"https://www.maryjok.com/",
+      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/Independence_Day_website_banner-01.jpg?v=1723477361',
+    },
+    {
+      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/desktop_banner_1.jpg?v=1715767477',
       url:"/collections/cult-accessories",
-      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/desktop_banner_1.jpg?v=1715767477'
     },
     {
       img:"https://cdn.shopify.com/s/files/1/0809/4253/0882/files/desktop_banner_3.jpg?v=1713342905",
@@ -29,9 +33,13 @@ const ImageCarousel = () => {
     }];
   }else{
     imageSrc = [{
-      url:"/collections/cult-accessories",
-      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/mobile_Hero_Banners_1.jpg?v=1715767503'
+      url:"https://www.maryjok.com/",
+      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/Independence_Day_website_banner-02.jpg?v=1723477360'
     },          
+    {
+      img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/mobile_Hero_Banners_1.jpg?v=1715767503',
+      url:"/collections/cult-accessories",
+    },
     {
       img:'https://cdn.shopify.com/s/files/1/0809/4253/0882/files/mobile_Hero_Banners_2.jpg?v=1713353171',
       url:"/collections/way-2-slay-mjk",
